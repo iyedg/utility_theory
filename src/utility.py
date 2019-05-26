667c2003ea4c7f640c7331cb6643dcb109a3ef43
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-05-26 14:42:56 +0100

    Normalized model to ensure values between 0 and 1

--- src/utility.py
+++ src/utility.py
@@ -26,44 +26,50 @@ class Utility:
     def from_assessment(cls, name):
         return cls(name=name)
 
-    def perfect_assess(self, vector, is_cost=False, method="least_squares"):
-        # TODO: check if it is cost
-        # TODO: check for when best and worst are not extrema
-        worst = vector.min()
-        best = vector.max()
-        if is_cost:
-            worst, best = best, worst
-        params = Parameters()
-        params.add("worst", value=worst, vary=False)
-        params.add("lower_middle", min=vector.min(), max=vector.max())
-        params.add("middle", min=vector.min(), max=vector.max())
-        params.add("upper_middle", min=vector.min(), max=vector.max())
-        params.add("best", value=best, vary=False)
-
-        def objective(params):
-            worst = params["worst"]
-            lower_middle = params["lower_middle"]
-            middle = params["middle"]
-            upper_middle = params["upper_middle"]
-            best = params["best"]
-            u = Utility.from_assessment(name="u")
-            x = np.array([worst, lower_middle, middle, upper_middle, best])
-            u.assess(points=x)
-            return u.fit().chisqr
-
-        return minimize(objective, params, method=method)
-
-    def assess(self, points=None, interactive=False):
+    def assess(
+        self,
+        points=None,
+        optimal_fit=False,
+        vector=None,
+        is_cost=False,
+        method="least_squares",
+    ):
+        # TODO: mutually exclusive args
         """Take in five points for direct assessment. When interactive
         is True, a Hint is displayed at each step of the assessment.
 
         Keyword Arguments:
             points {np.array | list-like} -- a list of five points (default: {None})
-            interactive {bool} -- Toggle for interactively setting the points (default: {False})
         """
-        # TODO: add a way to choose points optimally based
-        # on a vector to fit a model by minimizing a chi-square
-        self.points = points
+        # TODO: check if it is cost
+        # TODO: check for when best and worst are not extrema
+        if optimal_fit:
+            self.worst = vector.min()
+            self.best = vector.max()
+            if is_cost:
+                self.worst, self.best = self.best, self.worst
+            params = Parameters()
+            params.add("worst", value=self.worst, vary=False)
+            params.add("lower_middle", min=vector.min(), max=vector.max())
+            params.add("middle", min=vector.min(), max=vector.max())
+            params.add("upper_middle", min=vector.min(), max=vector.max())
+            params.add("best", value=self.best, vary=False)
+
+            def objective(params):
+                worst = params["worst"]
+                lower_middle = params["lower_middle"]
+                middle = params["middle"]
+                upper_middle = params["upper_middle"]
+                best = params["best"]
+                u = Utility.from_assessment(name="u")
+                x = np.array([worst, lower_middle, middle, upper_middle, best])
+                u.assess(points=x)
+                return u.fit().chisqr
+
+            self.optimal_fit = minimize(objective, params, method=method)
+            self.points = np.array(list(self.optimal_fit.params.valuesdict().values()))
+        else:
+            self.points = points
         return self.points
 
     @property
@@ -86,7 +92,7 @@ class Utility:
         )
         return self.result
 
-    def model(self, x, params=None, rounding=None):
+    def model(self, x, params=None):
         """
         Return an array of y values based on function calculated inside
         """
@@ -97,10 +103,23 @@ class Utility:
         c = params["c"]
 
         y = a + b ** (-c * x)
-        if rounding:
-            if isinstance(x, int) or isinstance(x, float):
-                return np.abs(np.round(y, 3))
         return y
+
+    def normalized_model(self, x, params=None):
+        """
+        Return an array of y values based on function calculated inside
+        Used only after optimality
+        """
+        if params is None:
+            params = self.result.params
+        a = params["a"]
+        b = params["b"]
+        c = params["c"]
+
+        lower = self.model(self.worst)
+        upper = self.model(self.best)
+        y = a + b ** (-c * x)
+        return (y - lower) / (upper - lower)
 
     def residuals(self, params, x, data):
         """
@@ -115,32 +134,10 @@ class Utility:
     def plot(self):
         trace1 = go.Scatter(x=self.points, y=np.linspace(0, 1, 5))
         x = np.linspace(self.points.min(), self.points.max(), 50)
-        y = self.model(x=x)
+        y = self.normalized_model(x=x)
         trace2 = go.Scatter(x=x, y=y)
         return [trace1, trace2]
 
     def __mul__(self, other):
-        kx, ky = diofant.symbols("k_x, k_y")
-        p, q = diofant.symbols("p, q")
-        ux = partial(self.model, rounding=True)
-        uy = partial(other.model, rounding=True)
-        mauf = lambda x, y: kx * ux(x) + ky * uy(y) + (1 - kx - ky) * ux(x) * uy(y)
-        # kx
-        l1 = mauf(self.points.max(), other.points.min())
-        l2 = p * mauf(self.points.max(), other.points.max()) + (1 - p) * mauf(
-            self.points.min(), other.points.min()
-        )
-        # ky
-        l3 = mauf(self.points.min(), other.points.max())
-        l4 = q * mauf(self.points.max(), other.points.max()) + (1 - q) * mauf(
-            self.points.min(), other.points.min()
-        )
-        # TODO: handle when no solution instead of getting first element directly
-        scaling_k = diofant.solve([l1 - l2, l3 - l4])[0]
-        mauf = (
-            lambda x, y: scaling_k.get(kx) * ux(x)
-            + scaling_k.get(ky) * uy(y)
-            + (1 - scaling_k.get(kx) - scaling_k.get(ky)) * ux(x) * uy(y)
-        )
-        return mauf
+        pass
 
